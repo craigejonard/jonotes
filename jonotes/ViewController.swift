7afbbb8d5ee2ac8f4ce64a5a31ec34a32ce48558
craigejonard <craige.fdc@gmail.com>
2025-08-27 13:32:56 +0800

    Initial Commit

---
+++ jonotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jonotes
+//
+//  Created by FDC.Jonard-NC-IOS on 8/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
